3433ee8d5418be7efaf73a226ac5278e12a08864
sabasharf123 <awsomesaba.8@gmail.com>
2017-07-20 10:13:58 -0700

    false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&rrnx4l7t*0%$vl7^v6y)ffjr@_vye6$60l8smt1xc(v9p(6be'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '<sabasharf>.pythonanywhere.com']
 
